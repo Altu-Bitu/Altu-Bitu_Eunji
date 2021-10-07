fee4fe000996f4e73eee689145e570f2be833b4e
Eunji <libera0129@gmail.com>
2021-10-07 18:07:41 +0900

    [그리디 알고리즘] 10월 7일 - update

--- 10월 1일 - 그리디 알고리즘/1448.cpp
+++ 10월 1일 - 그리디 알고리즘/1448.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int maxLengthSum(int n, vector<int>straw) {
 	int ans = 0;
 
-	for (int i = n - 1; i >= 2; i--) { //삼각형 만들기, 세 변의 길이의 합이 최대가 되어야 하므로 뒤쪽 인덱스부터 확인
-		if (straw[i] < straw[i - 1] + straw[i - 2]) { // 삼각형이 만들어진다면
-			ans = straw[i] + straw[i - 1] + straw[i - 2];
+	for (int i = 0; i < n-2; i++) { //삼각형 만들기
+		if (straw[i] < straw[i + 1] + straw[i + 2]) { // 삼각형이 만들어진다면
+			ans = straw[i] + straw[i + 1] + straw[i + 2];
 			return ans;
 		}
 	}
@@ -27,8 +27,8 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> straw[i];
 
-	//정렬
-	sort(straw.begin(), straw.end());
+	//정렬(내림차순)
+	sort(straw.begin(), straw.end(),greater<>());
 
 	//연산 + 출력
 	cout << maxLengthSum(n, straw) << '\n';
--- 10월 1일 - 그리디 알고리즘/16206.cpp
+++ 10월 1일 - 그리디 알고리즘/16206.cpp
@@ -3,68 +3,83 @@
 #include <algorithm>
 
 using namespace std;
+int m; //자를 수 있는 횟수
+
+//비교 함수
+bool compare(const int& a, const int& b) {
+	//10의 배수이면 오름차순
+	if (a % 10 == 0 && b % 10 == 0)
+		return a < b;
+
+	//10의 배수가 앞쪽으로 가도록
+	return (a % 10 == 0 && b % 10 != 0);
+}
 
 //자르는 함수
-void cutting(vector<int> &a, int &idx, int &cnt, int &m) {
-	while (a[idx] > 10) { //케이크가 10보다 크다면
-		a[idx] -= 10; // 10 만큼 자르기
-		cnt++; // 길이가 10인 케이크가 생겼으므로 증가
-		m--; //자를 수 있는 횟수는 감소
-		if (m == 0) //자를 수 없게 되면
-			break;
-	}
+int cutting(int a) {
+	int cnt = 0; //길이 10인 케이크의 개수
 
-	//다 자르고 난 결과
-	if (a[idx] == 10) { //10이면
-		cnt++; //케이크 개수 증가
-		idx++; //인덱스 증가
+	if (a % 10 == 0) { //10의 배수인 경우
+		if (a / 10 - 1 > m) { //자를 수 있는 횟수 이내에 케이크를 모두 10으로 분할할 수 없는 경우
+			cnt = m;
+			m = 0;
+		}
+		else {
+			cnt += a / 10;
+			m = m - a / 10 + 1;
+		}	
 	}
-	else { //10보다 작으면
-		idx++; //인덱스만 증가
+	else { //10의 배수가 아닌 경우
+		if (a / 10 > m) {
+			cnt = m;
+			m = 0;
+		}
+		else {
+			cnt += a / 10;
+			m = m - a / 10;
+		}
 	}
+	
+	
+	return cnt;
 }
 
 //케이크 개수 최댓값 구하는 함수
-int maxCakeCount(int m, vector<int>a, vector<int>multiple_of_10) {
+int maxCakeCount(vector<int>a) {
 	int cnt = 0;
-	int size1 = multiple_of_10.size(), size2 = a.size();
-	int idx1 = 0, idx2 = 0; //각각 multiple_of_10, a 배열 인덱스
+	int size = a.size();
+	int idx = 0;
 
-	while (m > 0 && idx1 < size1) {//자를 수 있고, 가지고 있는 케이크 개수를 넘어가지 않는 한 반복
-		cutting(multiple_of_10, idx1, cnt, m); //자르기
-	}
+	while (m > 0 && idx < size) {//자를 수 있고, 가지고 있는 케이크 개수를 넘어가지 않는 한 반복
+		if (a[idx] == 10) //길이가 10이면 자를 필요 없음
+			cnt++;
+		else 
+			cnt += cutting(a[idx]); //자르기
 
-	while (m > 0 && idx2 < size2) { //자를 수 있고, 가지고 있는 케이크 개수를 넘어가지 않는 한 반복
-		cutting(a, idx2, cnt, m);
+		idx++;
 	}
 
 	return cnt;
 }
 
 int main() {
-	int n, m;
+	int n;
 
 	//입력
 	cin >> n >> m;
 	vector<int>a;
-	vector<int>multiple_of_10;
 	for (int i = 0; i < n; i++) {
 		int input;
 		cin >> input;
 		if (input < 10) //10보다 작으면
 			continue;
-
-		if (input % 10 == 0) //10으로 나누어 떨어진다면
-			multiple_of_10.push_back(input);
-		else
-			a.push_back(input);
+		a.push_back(input);
 	}
 
 	//정렬
-	sort(a.begin(), a.end());
-	sort(multiple_of_10.begin(), multiple_of_10.end());
+	sort(a.begin(), a.end(), compare);
+
 
 	//연산 + 출력
-	cout << maxCakeCount(m, a, multiple_of_10) << '\n';
-
+	cout << maxCakeCount(a) << '\n';
 }
